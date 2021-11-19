1f2a2e1eb2af69b7198ad0cb4af4b7ef5f70e3e6
aaron.hoffer <github@hoffer.org>
2021-11-18 20:55:54 -0700

    I do not understand packages, modules, or imports

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # py-hole
+
 Simple Python HTTP interface to pi-hole
+
+## To Build
+
+`python3 -m build`
+
+
+## To Install From Files
+
+`pip3 install dist/py_hole-0.0.1-py3-none-any.whl`
---
+++ test/PyholeTest.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+from Pyhole import Pyhole
+from YouTubeRule import YouTubeRule
+
+
+class TestPyhole(unittest.TestCase):
+    pyhole = Pyhole('192.168.0.2', os.getenv('PI_PASSWD'))
+
+    def test_constructor(self):
+        self.assertIsNotNone(self.pyhole.host)
+        self.assertIsNotNone(self.pyhole.password)
+        self.assertIsNotNone(self.pyhole.session)
+
+    def test_authentication(self):
+        self.pyhole.authenticate()
+
+    def test_bad_token(self):
+        self.pyhole.token = 'expired'
+        x = self.pyhole.get_domains()
+        pass
+
+    def test_get_domains(self):
+        self.pyhole.authenticate()
+        x = self.pyhole.get_domains()
+        pass
+
+    def test_youtube(self):
+        yt_rule = YouTubeRule(self.pyhole)
+        is_blocked = yt_rule.youtube_is_blocked()
+        yt_rule.flip()
+        new_status = yt_rule.youtube_is_blocked()
+        self.assertNotEqual(is_blocked, new_status)
+
+
+# TODO: Test multiple groups
+
+if __name__ == '__main__':
+    unittest.main()
