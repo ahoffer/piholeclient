8a9a80e73094b40bf10178d1142563b55292429b
aaron.hoffer <aaron.hoffer@connexta.com>
2021-12-06 16:40:35 -0700

    Remove print statement. A couple of refactorings.

--- src/piholeclient/controllers.py
+++ src/piholeclient/controllers.py
@@ -6,34 +6,32 @@ class YouTubeRule:
     def __init__(self, pihole):
         self.pihole = pihole
 
-    def _getYouTubeRule(self):
+    def _get_youtube_rule(self):
         domainlist = self.pihole.get_domains()
         yt_rules = [x for x in domainlist if 'youtube' in x.domain and x.type is ListType.regex_blacklist]
         return next(iter(yt_rules), None)
 
     def youtube_is_blocked(self):
-        rule = self._getYouTubeRule()
+        rule = self._get_youtube_rule()
         return rule is not None and rule.enabled
 
     def flip(self):
-        rule = self._getYouTubeRule()
+        rule = self._get_youtube_rule()
         if rule:
             rule.flip()
-            r = self.pihole.edit_domain(rule)
-            print(r.text)
+            self.pihole.edit_domain(rule)
         else:
             new_rule = DomainItem({})
             new_rule.type = ListType.regex_blacklist
             new_rule.enabled = True
             new_rule.comment = 'Added by piholeclient python package'
             new_rule.domain = '.*youtube.*'
-            r = self.pihole.add_domain(new_rule)
-            print(r.text)
+            self.pihole.add_domain(new_rule)
 
     def block(self):
         if not self.youtube_is_blocked():
-            flip()
+            self.flip()
             
     def unblock(self):
         if self.youtube_is_blocked():
-            flip()
+            self.flip()
